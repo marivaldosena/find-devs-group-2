9d032bd05bee022d2a6d21ac565cf57c01ffa056
Marivaldo Sena <marivaldo.sena@live.com>
2022-09-14 19:56:35 -0300

    Fix bug related to SignIn page.

--- src/navigation/index.jsx
+++ src/navigation/index.jsx
@@ -64,17 +64,15 @@ const Navigation = () => {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         {user ? (
-          <Stack.Screen name="SignIn" component={SignIn} />
-
+          <Stack.Screen name="Home" component={Home} />   
         ) : (
           <>
+            <Stack.Screen name="SignIn" component={SignIn} />
             <Stack.Screen name="SignUp" component={SignUp} />
             <Stack.Screen name="ConfirmEmail" component={Confirm} />
             <Stack.Screen name="ForgotPassword" component={Forgot} />
             <Stack.Screen name="NewPassword" component={Reset} />
-            <Stack.Screen name="SignIn" component={SignIn} />
             <Stack.Screen name="Favs" component={Favs} />
-            <Stack.Screen name="Home" component={Home} />   
           </>
         )}
       </Stack.Navigator>
