4b88399e55a83e7b683ec661ec088a37840c0419
andre <andre.decarvalho1@gmail.com>
2022-09-15 15:03:27 -0300

    update styles

--- src/screens/Reset/styles.js
+++ src/screens/Reset/styles.js
@@ -56,7 +56,15 @@ export default StyleSheet.create({
         justifyContent:'center',
         backgroundColor: '#2D9135',
         borderRadius: 6,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.20,
+        shadowRadius: 1.41,
 
+        elevation: 2,
     },
 
     icon:{
