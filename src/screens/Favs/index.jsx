bbbaa19da89970a9f35405c27990599b205cddb6
LucianoNeo <tec.info.luciano@hotmail.com>
2022-09-15 18:21:17 -0300

    favorites working

--- src/screens/Favs/index.jsx
+++ src/screens/Favs/index.jsx
@@ -5,56 +5,52 @@ import { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { AntDesign, Entypo, FontAwesome5, Ionicons, MaterialIcons } from '@expo/vector-icons';
 import { Header } from '../../components/Header';
-
+import { useSelector, useDispatch } from 'react-redux'
+import { favStateData, addNewFavorite, removeFavorite } from '../../store/modules/favorites/reducer'
+import { CardDev } from '../../components/CardDev';
 
 export default function Favs() {
 
-  const [username, setUser] = useState('')
-  const [code, setCode] = useState('')
-  const [password, setPassword] = useState('')
-  const [loading, setLoading] = useState(false)
+    const [username, setUser] = useState('')
+    const [code, setCode] = useState('')
+    const [password, setPassword] = useState('')
+    const [loading, setLoading] = useState(false)
+    const favorite = useSelector(favStateData);
 
+    const onResendPress = async () => {
+        if (loading) {
+            return
+        }
+    }
 
-  const onResendPress = async () => {
-      if (loading) {
-          return
-      }    
-  }
+    return (
+        <>
+            <Header onpress='home' />
+            <View style={styles.container}>
+                <View style={styles.hello}>
+                    <Text style={styles.title}>Favoritos</Text>
+                </View>
+                {favorite === [] &&
+                <View style={styles.container}>
+                <Text style={styles.texts}>Não há favoritos salvos.</Text>
+                </View>
+                }
+                {favorite.map((item,index)=>(
+                    <CardDev
+                    key={item.id}
+                    id={item.id}
+                    name={item.name}
+                    stack={item.stack}
+                    category={item.category}
+                    state={item.state}
+                    photo={item.photo}
+                    description={item.description}
+                  />
+                ))}
 
-  return (
-      <>
-          <Header onpress='home'/>
-          <View style={styles.container}>
-              <View style={styles.hello}>
-                  <Text style={styles.title}>Favoritos</Text>
-              </View>
+                
 
-
-               <View style={styles.card}>
-                  <View style={styles.profile}>
-                      <View style={styles.cardicon}>
-                          <Ionicons style={styles.cardicon} name="person-circle-outline" size={40} color="#EEE" />
-                      </View>
-                  </View>
-
-                  <View style={styles.cardinfo}>
-                      
-                          <Text style={styles.titleboldfav}>Fernanda Peters</Text>
-                          <View style={styles.cardstacks}>
-                              <Text style={styles.javascriptstack}>JavaScript, </Text>
-                              <Text style={styles.reactstack}>React, </Text>
-                              <Text style={styles.reactnativestack}>React Native</Text>
-                          </View>
-                      
-                  </View>
-
-                  <View>
-                      <Ionicons style={styles.iconfav} name="star" size={20} color="#DE8F45" />
-                  </View>
-
-              </View>
-                           
-          </View>
-      </>
-  )
+            </View>
+        </>
+    )
 }
--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -6,7 +6,7 @@ import { CardDev } from '../../components/CardDev';
 import { useNavigation } from '@react-navigation/native';
 import { Header } from '../../components/Header';
 
-import {listDevelopers} from '../../services/devApi'
+import { listDevelopers } from '../../services/devApi'
 
 
 export default function Home() {
@@ -15,84 +15,85 @@ export default function Home() {
 
   const [search, setSearch] = useState('')
   const [loading, setLoading] = useState(false)
-  const [devs,setDevs] = useState(listDevelopers)
+  const [devs, setDevs] = useState(listDevelopers)
 
   const devEx = {
-      category: "Front-end",
-      description: "Desenvolvedor a x anos",
-      id: 5,
-      name: "Junior da Silva",
-      photo: "https://dpbnri2zg3lc2.cloudfront.net/en/wp-content/uploads/2021/01/Sam-CF.jpg",
-      stack: "React",
-      state: "São Paulo",
+    id: 5,
+    name: "Junior da Silva",
+    stack: "React",
+    category: "Front-end",
+    state: "São Paulo",
+    photo: "https://dpbnri2zg3lc2.cloudfront.net/en/wp-content/uploads/2021/01/Sam-CF.jpg",
+    description: "Desenvolvedor a x anos",
+
   }
 
 
-useEffect(() => {
-  if (!devs){
-    return
-  }
-  console.log(devs)
-}, [devs])
+  useEffect(() => {
+    if (!devs) {
+      return
+    }
+    console.log(devs)
+  }, [devs])
 
   return (
     <>
 
-      <Header onpress='signout'/>
-      {!devs ? 
-      <View style={styles.container}>
-      <ActivityIndicator/>
-      </View>
-      :
-      <View style={styles.container}>
-        <View style={styles.hello}>
-          <Text style={styles.title}>Olá,</Text>
-          <Text style={styles.titleBold}>Dev</Text>
-          <Text style={styles.title}>!</Text>
+      <Header onpress='signout' />
+      {!devs ?
+        <View style={styles.container}>
+          <ActivityIndicator />
         </View>
-     
+        :
+        <View style={styles.container}>
+          <View style={styles.hello}>
+            <Text style={styles.title}>Olá,</Text>
+            <Text style={styles.titleBold}>Dev</Text>
+            <Text style={styles.title}>!</Text>
+          </View>
 
-        <View style={styles.form}>
+
+          <View style={styles.form}>
 
 
-          <View style={styles.inputContainer}>
+            <View style={styles.inputContainer}>
 
-            <TextInput
-              autoCompleteType='off'
-              style={styles.inputs}
-              placeholderTextColor='#EEE'
-              placeholder='Digite para pesquisar... '
-              onChangeText={value => setSearch(value)}
-            />
-            <View style={styles.icon}>
-              <Ionicons name="ios-search" size={16} color="#EEE" />
+              <TextInput
+                autoCompleteType='off'
+                style={styles.inputs}
+                placeholderTextColor='#EEE'
+                placeholder='Digite para pesquisar... '
+                onChangeText={value => setSearch(value)}
+              />
+              <View style={styles.icon}>
+                <Ionicons name="ios-search" size={16} color="#EEE" />
+              </View>
             </View>
           </View>
-        </View>
 
-        <TouchableOpacity
-          onPress={() => navigate('Favs')}
-          disabled={loading ? true : false}
-          style={styles.buttons}
-        >
-          <Text style={styles.texts}>
-            {loading ? 'Carregando...' : 'Favoritos'}
-          </Text>
-        </TouchableOpacity>
-      <CardDev 
-      id={devEx.id} 
-      name={devEx.name} 
-      stack={devEx.stack} 
-      category={devEx.category} 
-      state={devEx.state} 
-      photo={devEx.photo}
-      description={devEx.description}
-      />
+          <TouchableOpacity
+            onPress={() => navigate('Favs')}
+            disabled={loading ? true : false}
+            style={styles.buttons}
+          >
+            <Text style={styles.texts}>
+              {loading ? 'Carregando...' : 'Favoritos'}
+            </Text>
+          </TouchableOpacity>
+          <CardDev
+            id={devEx.id}
+            name={devEx.name}
+            stack={devEx.stack}
+            category={devEx.category}
+            state={devEx.state}
+            photo={devEx.photo}
+            description={devEx.description}
+          />
 
-    
 
-        
-      </View>}
+
+
+        </View>}
 
 
     </>
